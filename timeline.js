9f7127e4a60170ba8c60ceb9015680c8b0fe6c0a
Andrew McOlash <amcolash@gmail.com>
2017-12-27 22:46:14 -0600

    Add in reading exif, writing gps coords

--- timeline.js
+++ timeline.js
@@ -1,76 +1,134 @@
 #!/usr/bin/env node
 
-var lerp = require('lerp');
-var moment = require('moment');
-var tj = require('@mapbox/togeojson');
-var fs = require('fs');
-var DOMParser = require('xmldom').DOMParser;
+const lerp = require('lerp');
+const moment = require('moment');
+const tj = require('@mapbox/togeojson');
+const fs = require('fs');
+const DOMParser = require('xmldom').DOMParser;
+const exiftool = require('node-exiftool');
+const ep = new exiftool.ExiftoolProcess();
 
-var args = [];
+var converted; // gets defined after kml is loaded
 
-for (var i = 0; i < process.argv.length; i++) {
-  var arg = process.argv[i];
-  if (!arg.includes("/node")) {
-    args.push(arg);
+// Kick things off
+checkArgs();
+
+function checkArgs() {
+  var args = [];
+
+  for (var i = 0; i < process.argv.length; i++) {
+    var arg = process.argv[i];
+    if (!arg.includes("/node")) {
+      args.push(arg);
+    }
+  }
+
+  if (args.length < 3 || !args[1].endsWith(".kml")) {
+    console.error("usage: timeline.js [locations.kml] [image file]");
+    process.exit(1);
+  }
+
+  // Start loading now
+  loadKml(args[1], args[2]);
+}
+
+function loadKml(path, image) {
+  var kml = new DOMParser().parseFromString(fs.readFileSync(path, 'utf8'));
+  converted = tj.kml(kml);
+
+  var coords;
+
+  ep.open()
+    .then(() => ep.readMetadata(image, ['-File:all']))
+    .then((res) => {
+      var time = getTime(res.data[0]);
+      coords = getCoordinates(time);
+    })
+    .then(() => {
+      if (coords) {
+        console.log(coords);
+        ep.writeMetadata(image, {
+          GPSLongitudeRef: 'W',
+          GPSLongitude: coords.lng,
+          GPSLatitudeRef: 'N',
+          GPSLatitude: coords.lat
+        });
+      }
+    })
+    .then(() => ep.close())
+}
+
+function getTime(data) {
+  var dataTime = data.DateTimeOriginal || data.CreateDate || data.ModifyDate;
+  if (dataTime) {
+    // Assuming format 'YYYY:MM:DD hh:mm:ss'
+    var parts = dataTime.split(" ");
+    parts[0] = parts[0].replace(/:/g, "-");
+    var time = moment.utc(parts[0] + "T" + parts[1] + "Z");
+    
+    if (data.TimeZone) {
+      // Assuming format '+-hh:mm'
+      var splitZone = data.TimeZone.split(":");
+      var sign = Math.sign(splitZone[0]);
+
+      time = time.add(Math.abs(splitZone[0]), 'h');
+      time = time.add(splitZone[1], 'm');
+    }
+
+    // Doing the search in UTC to match to timeline format
+    console.log("Search time: " + time.toString());
+
+    return time;
   }
 }
 
-if (args.length < 2) {
-  console.error("usage: timeline.js [locations.json] [(Optional) timezone offset]");
-  process.exit(1);
-}
+function getCoordinates(searchTime) {
 
-var kml = new DOMParser().parseFromString(fs.readFileSync(args[1], 'utf8'));
-var converted = tj.kml(kml);
+  for (var i = 0; i < converted.features.length; i++) {
+    var feature = converted.features[i];
+    var start = moment.utc(feature.properties.timespan.begin);
+    var end = moment.utc(feature.properties.timespan.end);
+    
+    
+    if (!searchTime || searchTime.isBetween(start, end) || searchTime.isSame(start) || searchTime.isSame(end)) {
+      if (!searchTime) {
+        console.log("--------------------------------");
+        console.log(feature.properties.name);
+        console.log(start.toString());
+        console.log(end.toString());
+      }
 
-if (args.length > 2) {
-  var offset = args[2];
-  console.log(offset)
-}
+      // console.log("Found start time: " + start.toString());
+      // console.log("Found end time: " + end.toString());
 
-var searchDate;
-searchDate = moment('2017-12-26T17:30:19-06:00'); // comment out to disable searching
-searchDate = moment('2017-12-26T17:33:37-06:00');
+      for (var j = 0; j < feature.geometry.geometries.length; j++) {
+        var geo = feature.geometry.geometries[j];
+        if (geo.type === "LineString") {
+          var totalDuration = end.diff(start);
+          var partialDuration = searchTime.diff(start);
+          var percent = partialDuration / totalDuration;
 
-for (var i = 0; i < converted.features.length; i++) {
-  var feature = converted.features[i];
-  var start = moment(feature.properties.timespan.begin);
-  var end = moment(feature.properties.timespan.end);
-  
-  
-  if (!searchDate || searchDate.isBetween(start, end) || searchDate.isSame(start) || searchDate.isSame(end)) {
-    if (!searchDate) {
-      console.log("--------------------------------");
-      console.log(feature.properties.name);
-      console.log(start.toString());
-      console.log(end.toString());
-    }
+          if (geo.coordinates.length === 1) {
+            // Simple case
+            var lat = geo.coordinates[0][1];
+            var lng = geo.coordinates[0][0];
+          } else {
+            // If we have more than one point, we are assuming that we are moving at a constant velocity
 
-    for (var j = 0; j < feature.geometry.geometries.length; j++) {
-      var geo = feature.geometry.geometries[j];
-      if (geo.type === "LineString") {
-        var totalDuration = end.diff(start);
-        var partialDuration = searchDate.diff(start);
-        var percent = partialDuration / totalDuration;
+            // Find which coordinates to lerp between
+            var index = percent * (geo.coordinates.length - 1);
+            var fraction = index - Math.floor(index);
+            index = Math.floor(index); // floor index now
 
-        if (geo.coordinates.length === 1) {
-          // Simple case
-          var lat = geo.coordinates[0][0];
-          var lng = geo.coordinates[0][1];
-        } else {
-          // If we have more than one point, we are assuming that we are moving at a constant velocity
+            // Lerp between closest points
+            var lat = lerp(geo.coordinates[index][1], geo.coordinates[index + 1][1], fraction);
+            var lng = lerp(geo.coordinates[index][0], geo.coordinates[index + 1][0], fraction);
+          }
 
-          // Find which coordinates to lerp between
-          var index = percent * (geo.coordinates.length - 1);
-          var fraction = index - Math.floor(index);
-          index = Math.floor(index); // floor index now
+          console.log(lat + ", " + lng);
 
-          // Lerp between closest points
-          var lat = lerp(geo.coordinates[index][0], geo.coordinates[index + 1][0], fraction);
-          var lng = lerp(geo.coordinates[index][1], geo.coordinates[index + 1][1], fraction);
+          return { lat: lat, lng: lng };
         }
-
-        console.log(lat + ", " + lng);
       }
     }
   }
